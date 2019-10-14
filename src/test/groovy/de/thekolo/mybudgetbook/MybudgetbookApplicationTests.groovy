8b8cf0c1e1145b7de0d8e7e19cac411cb8755984
Patrick Kolodziej <koloooo91@googlemail.com>
2019-10-14 22:11:41 +0200

    Setup MockMvc

--- src/test/groovy/de/thekolo/mybudgetbook/MybudgetbookApplicationTests.groovy
+++ src/test/groovy/de/thekolo/mybudgetbook/MybudgetbookApplicationTests.groovy
@@ -2,13 +2,19 @@ package de.thekolo.mybudgetbook
 
 import de.thekolo.mybudgetbook.repositories.AccountRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class MybudgetbookApplicationTests extends Specification {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Autowired
     AccountRepository accountRepository
