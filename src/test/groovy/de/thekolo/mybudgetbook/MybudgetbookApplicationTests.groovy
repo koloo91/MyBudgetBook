40f144b3590d91578310d68d157df2e64f6a1298
Patrick Kolodziej <koloooo91@googlemail.com>
2019-10-14 21:16:05 +0200

    Setup spock tests

---
+++ src/test/groovy/de/thekolo/mybudgetbook/MybudgetbookApplicationTests.groovy
@@ -0,0 +1,18 @@
+package de.thekolo.mybudgetbook
+
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class MybudgetbookApplicationTests extends Specification {
+
+
+    def "dummy spec"() {
+        when:
+        true
+
+        then:
+        true
+    }
+}
