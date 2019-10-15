bbc3cfdc726b4c207d261e2e1dc08ead788e84e5
koloo91 <koloooo91@googlemail.com>
2019-10-15 14:08:12 +0000

    AccountController

--- src/main/java/de/thekolo/mybudgetbook/controller/AccountController.java
+++ src/main/java/de/thekolo/mybudgetbook/controller/AccountController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.thekolo.mybudgetbook.models.Account;
 import de.thekolo.mybudgetbook.models.AccountDto;
 
 @RestController
