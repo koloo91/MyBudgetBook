23c496c5c3ca49d7a043176141214f79e950819b
Patrick Kolodziej <koloooo91@googlemail.com>
2019-10-22 19:34:59 +0200

    Configure env

--- backend/main.go
+++ backend/main.go
@@ -10,10 +10,18 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/koloo91/controller"
 	"log"
+	"os"
+)
+
+var (
+	dbHost     = getEnvOrDefault("DB_HOST", "localhost")
+	dbUser     = getEnvOrDefault("DB_USER", "postgres")
+	dbPassword = getEnvOrDefault("DB_PASSWORD", "")
+	dbName     = getEnvOrDefault("DB_NAME", "postgres")
 )
 
 func main() {
-	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", "postgres", "", "localhost", 5432, "postgres")
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", dbUser, dbPassword, dbHost, 5432, dbName)
 	db, err := gorm.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatalf("unable to open database connection: %s", err.Error())
@@ -41,4 +49,11 @@ func main() {
 	router.Static("/", "./assets")
 
 	log.Fatal(router.Run())
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
