2224bb8509cbf41b16a9e3d451f64d3d3664393f
Patrick Kolodziej <koloooo91@googlemail.com>
2019-10-24 19:50:02 +0200

    Display categories

--- frontend/src/app/bookings/bookings.component.ts
+++ frontend/src/app/bookings/bookings.component.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 import {Booking} from '../models/booking.model';
 import {MatDialog} from '@angular/material/dialog';
 import {CreateBookingDialogComponent} from './create-booking-dialog/create-booking-dialog.component';
+import {Category} from '../models/category.model';
+import {CategoryService} from '../services/category.service';
 
 @Component({
   selector: 'app-bookings',
@@ -14,17 +16,27 @@ import {CreateBookingDialogComponent} from './create-booking-dialog/create-booki
 export class BookingsComponent implements OnInit {
 
   bookings: Observable<Booking[]>;
+  categories: Observable<Category[]>;
 
   constructor(private bookingService: BookingService,
+              private categoryService: CategoryService,
               public dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.loadBookings();
+    this.loadCategories();
   }
 
   private loadBookings() {
     this.bookings = this.bookingService.getBookings()
+      .pipe(
+        map(pagedEntity => pagedEntity.content)
+      );
+  }
+
+  private loadCategories() {
+    this.categories = this.categoryService.getCategories()
       .pipe(
         map(pagedEntity => pagedEntity.content)
       );
@@ -39,5 +51,12 @@ export class BookingsComponent implements OnInit {
       console.log(result);
       this.loadBookings();
     });
+  }
+
+  getCategory(categoryId: string): Observable<string> {
+    return this.categories.pipe(
+      map(categories => categories.find(_ => _.id === categoryId)),
+      map(_ => _.id)
+    );
   }
 }
