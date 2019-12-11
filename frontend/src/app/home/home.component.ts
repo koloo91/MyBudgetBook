84a2b960335ba04d65f49dfc76bc0608ece8c6ef
Patrick Kolodziej <koloooo91@googlemail.com>
2019-12-11 21:53:48 +0100

    Close drawer on element click

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,16 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
   title = 'My Budget Book';
 
-  constructor() {
+  @ViewChild('drawer', {static: false})
+  private matDrawer: MatDrawer;
+
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.matDrawer.close();
+    })
   }
 
 }
